da66e91d11bae468e8e7ea92964b03edc1ba012c
Lisa Collins <Lisa.Collins@veteransunited.com>
2025-02-07 08:50:20 -0600

    lisa absolute calculate

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -2,6 +2,10 @@ def calculateAbsolute():
     
     # This first line is provided for you
     in_num  = input("Enter a number: ")
+    if in_num > 21:
+        print("Result: "+str(2*(in_num-21)))
+    else:
+        print("Result: "+str(21-in_num))
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
